476163ec9ea46663a2efd7f10167503720490677
Edgar Albalate <dev.gardo@gmail.com>
2019-02-05 16:34:21 +0100

    Hided sqrt function in module

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from math import sqrt
+from math import sqrt as _sqrt
 
 def hypotenuse(a, b):
-  return sqrt(a**2 + b**2)
+  return _sqrt(a**2 + b**2)
 
 def cathetus(h, c):
-  return sqrt(h**2 - c**2)
+  return _sqrt(h**2 - c**2)
 
 if __name__ == "__main__":
   print("(f) hypotenuse(3, 4): %f" % hypotenuse(3, 4))
