e9f4274004e6d888f150db37b993e6dcbe14f223
Edgar Albalate <dev.gardo@gmail.com>
2019-02-05 17:22:52 +0100

    Added some documentation to main file. Made some code cleanup.

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+"""Pythagoras theorem
+
+This module contains some functions from the pythagorean theorem.
+"""
+
 from math import sqrt as _sqrt
 
 def hypotenuse(a: float, b: float) -> float:
+  "Returns the hypotenuse of the triangle described by the a and b cathetus."
   return _sqrt(a**2 + b**2)
 
-def cathetus(h: float, c:float) -> float:
+def cathetus(h: float, c: float) -> float:
+  "Returns the cathetus of the triangle described by the hypothenuse (h) and the other cathetus (c)."
   return _sqrt(h**2 - c**2)
 
 if __name__ == "__main__":
