9e2a32a203974ba7ccc2fab63634deeec74a1c01
Edgar Albalate <dev.gardo@gmail.com>
2019-02-05 16:23:11 +0100

    Some code cleanup

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from math import sqrt
 
 def hypotenuse(a, b):
-  return sqrt( a**2 + b**2)
+  return sqrt(a**2 + b**2)
 
 def cathetus(h, c):
   return sqrt(h**2 - c**2)
-  pass
 
 if __name__ == "__main__":
   print("(f) hypotenuse(3, 4): %f" % hypotenuse(3, 4))
