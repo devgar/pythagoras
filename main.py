f14094031500ea0d6debb66639be63bde7e8664a
Edgar Albalate <dev.gardo@gmail.com>
2019-02-05 16:22:26 +0100

    Cathetus function implemented

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ def hypotenuse(a, b):
   return sqrt( a**2 + b**2)
 
 def cathetus(h, c):
+  return sqrt(h**2 - c**2)
   pass
 
 if __name__ == "__main__":
   print("(f) hypotenuse(3, 4): %f" % hypotenuse(3, 4))
+  print("(f) cathetus(5, 3):   %f" % cathetus(5, 3))
